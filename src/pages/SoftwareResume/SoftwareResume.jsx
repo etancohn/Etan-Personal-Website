d0d4de6b700556ec3346a3d3de7a4fbbea7c232a
Etan Cohn <etancohn@Etans-Air.lan>
2023-06-09 14:41:53 -0500

    debugging resume photo on netlify

--- src/pages/SoftwareResume/SoftwareResume.jsx
+++ src/pages/SoftwareResume/SoftwareResume.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './SoftwareResume.css'
 import GreenNavbar from '../../components/GreenNavbar'
+import softwareResumePhoto from '../../pics/software_resume.jpg'
 
 function SoftwareResume() {
   return (
@@ -8,7 +9,8 @@ function SoftwareResume() {
         <GreenNavbar />
         <div className="software-page-content">
             <h1 className="software-resume-title">Software Resume</h1>
-            <img src="/src/pics/software_resume.jpg" alt="Etan's software resume" 
+            {/* <img src="/src/pics/software_resume.jpg" alt="Etan's software resume"  */}
+            <img src={softwareResumePhoto} alt="Etan's software resume" 
                 className="resume-img"/>
         </div>
     </div>
