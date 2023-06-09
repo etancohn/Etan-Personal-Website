0f45ffe743150c9526336eaa233148957f30d7d1
Etan Cohn <etancohn@Etans-Air.lan>
2023-06-09 14:45:56 -0500

    fixed linking to resume page from navbar

--- src/pages/SoftwareResume/SoftwareResume.jsx
+++ src/pages/SoftwareResume/SoftwareResume.jsx
@@ -9,7 +9,6 @@ function SoftwareResume() {
         <GreenNavbar />
         <div className="software-page-content">
             <h1 className="software-resume-title">Software Resume</h1>
-            {/* <img src="/src/pics/software_resume.jpg" alt="Etan's software resume"  */}
             <img src={softwareResumePhoto} alt="Etan's software resume" 
                 className="resume-img"/>
         </div>
