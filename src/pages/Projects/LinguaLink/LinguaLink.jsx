1e01290264942369e9116e4e54cf5456252a9a1c
Etan Cohn <etan.cohn@gmail.com>
2023-07-06 10:53:22 -0500

    added history with colored squares, uses state

--- src/pages/Projects/LinguaLink/LinguaLink.jsx
+++ src/pages/Projects/LinguaLink/LinguaLink.jsx
@@ -8,6 +8,8 @@ import logo2 from '../../../pics/LinguaLink/logo_2.png'
 import logo3 from '../../../pics/LinguaLink/logo_3.png'
 import logo4 from '../../../pics/LinguaLink/logo_4.png'
 import logo5 from '../../../pics/LinguaLink/logo_5.png'
+import main_logo from '../../../pics/LinguaLink/main_logo.png'
+import SideBar from './SideBar.jsx'
 
 // API Key for authentication
 const API_KEY = import.meta.env.VITE_OPENAI_API_KEY
@@ -104,6 +106,18 @@ function extractValues(outputText, setTranslation, setAssociation, setMentalImag
     setExplanation(explanation);
 }
 
+function historyAddWord (word, translation, association, mentalImage, explanation, outputText, setHistory) {
+    const newVal = {
+        word: word,
+        translation: translation,
+        association: association,
+        mentalImage: mentalImage,
+        explanation: explanation,
+        outputText: outputText
+    }
+    setHistory(prevHistory => [...prevHistory, newVal])
+}
+
 // html for a single row of output
 function LinguaSingleOutput( {logo=logo1, title="TITLE", outputText="text.", isEven=false, num=0} ) {
     return (
@@ -120,9 +134,9 @@ function LinguaSingleOutput( {logo=logo1, title="TITLE", outputText="text.", isE
 }
 
 function LinguaLink() {
-  const [vocabWord, setVocabWord] = React.useState("")
   const [outputText, setOutputText] = React.useState("Enter a vocab word above!");
   const [outputValidationStr, setOutputValidationStr] = React.useState("");
+  const [vocabWord, setVocabWord] = React.useState("")
   const [translation, setTranslation] = React.useState("");
   const [association, setAssociation] = React.useState("");
   const [mentalImage, setMentalImage] = React.useState("");
@@ -131,8 +145,48 @@ function LinguaLink() {
   const [vocabWordInputFocussed, setVocabWordInputFocussed] = React.useState(false);
   const vocabWordInputRef = React.useRef(null);
 
+  function updateOutput (i) {
+    const updated = history[i]
+    setVocabWord(updated.word)
+    setOutputValidationStr(updated.word)
+    setOutputText(updated.outputText)
+  }
+
+  const initialHistory = [
+    {
+        word: "invierno",
+        translation: "winter",
+        association: "'In Vino' (In wine)",
+        mentalImage: "Imagine being inside a cozy cabin during winter, sitting by the fireplace with a glass of red wine.",
+        explanation: `The association 'In Vino' sounds similar to 'invierno,' which means winter in Spanish. The mental image 
+                    of enjoying wine inside the cabin during winter helps to anchor the association and make it easier to 
+                    remember the Spanish vocabulary word.`
+    },
+    {
+        word: "ajedrez",
+        translation: "chess",
+        association: "'a jestress'",
+        mentalImage: "Imagine a funny jestress (female jester) playing chess with oversized pieces on a giant chessboard.",
+        explanation: `I associate "ajedrez" with "a jestress" because both words sound similar. The mental image of the funny 
+                    jestress playing chess helps me remember the Spanish word for chess.`
+    },
+    {
+        word: "lavadora",
+        translation: "washing machine",
+        association: "avalanche",
+        mentalImage: "Imagine an avalanche of clothes pouring out of a washing machine.",
+        explanation: `The word "lavadora" sounds similar to "avalanche." By creating a mental image of an avalanche of clothes pouring 
+                    out of a washing machine, we can easily remember the association between "lavadora" and a washing machine. The 
+                    exaggerated and memorable image helps with elaborative encoding, making the association more memorable.`
+    }
+  ]
+  const [history, setHistory] = React.useState([])
+
   React.useEffect(() => {
     extractValues(outputText, setTranslation, setAssociation, setMentalImage, setExplanation);
+    if (vocabWord.length > 0) {
+        historyAddWord(vocabWord, translation, association, mentalImage, explanation, outputText, setHistory)
+    }
   }, [outputText])
 
   React.useEffect(() => {
@@ -164,48 +218,65 @@ function LinguaLink() {
     <>
     <div className="lingua-link-content">
         <GreenNavbar />
-        <div className="lingua-link-content-container">
-
-            {/* title and description */}
-            <h1 className="project-title">Lingua Link</h1>
-            <p className="project-description">Elevate your Spanish vocabulary learning experience! Lingua Link employs powerful memory techniques to enhance your ability to learn, remember, and effortlessly recall new Spanish vocab words.</p>
-
-            {/* user input for vocab word */}
-            <div className="vocab-input-container">
-                <h4 className="project-mini-title">Vocab Word</h4>
-                <input 
-                    className="vocab-word-input"
-                    type="text" 
-                    placeholder="ex: invierno" 
-                    value={vocabWord}
-                    ref={vocabWordInputRef}
-                    onFocus={() => setVocabWordInputFocussed(true)}
-                    onBlur={() => setVocabWordInputFocussed(false)}
-                    onChange={(e) => setVocabWord(e.target.value)}>
-                </input>
-                <button 
-                    className="submit-btn"
-                    onClick={() => makeGPTCall(vocabWord, setOutputText, setOutputValidationStr)}>
-                        Submit
-                </button>
+        <div className="lingua-link-columns">
+            <div className="lingua-link-sidebar">
+                <SideBar history={history} updateOutput={updateOutput} />
             </div>
 
-            <div className="output-box-container">
-                <LinguaSingleOutput logo={logo1} title="YOUR WORD" outputText={outputValidationStr} isEven={true} num="1" />
-                <LinguaSingleOutput logo={logo2} title="TRANSLATION" outputText={translation} num="2" />
-                <LinguaSingleOutput logo={logo3} title="ASSOCIATION" outputText={association} isEven={true} num="3" />
-                <LinguaSingleOutput logo={logo4} title="MENTAL IMAGE" outputText={mentalImage} num="4" />
-                <LinguaSingleOutput logo={logo5} title="EXPLANATION" outputText={explanation} isEven={true} num="5" />
+            <div className="non-sidebar">
+                <div className="lingua-link-content-container">
+                    {/* title and description */}
+                    {/* <h1 className="project-title">Lingua Link</h1> */}
+                    <div className="logo-and-description">
+                        <img src={main_logo} alt="main Lingua Link logo" className="ll-main-logo" />
+                        <p className="ll-project-description">
+                            Elevate your Spanish vocabulary learning experience! Lingua Link deploys powerful memory techniques 
+                            to enhance your ability to learn, remember, and effortlessly recall new Spanish vocab words.
+                        </p>
+                    </div>
+
+                    {/* user input for vocab word */}
+                    <div className="vocab-input-container">
+                        <h4 className="project-mini-title ll-title">VOCAB WORD</h4>
+                        <input 
+                            className="vocab-word-input"
+                            type="text" 
+                            placeholder="ex: invierno" 
+                            value={vocabWord}
+                            ref={vocabWordInputRef}
+                            onFocus={() => setVocabWordInputFocussed(true)}
+                            onBlur={() => setVocabWordInputFocussed(false)}
+                            onChange={(e) => setVocabWord(e.target.value)}>
+                        </input>
+                        <button 
+                            className="submit-btn ll-btn"
+                            onClick={() => makeGPTCall(vocabWord, setOutputText, setOutputValidationStr)}>
+                                Submit
+                        </button>
+                    </div>
+
+                    <div className="ll-output-box-container">
+                        <LinguaSingleOutput logo={logo1} title="YOUR WORD" outputText={outputValidationStr} isEven={true} num="1" />
+                        <LinguaSingleOutput logo={logo2} title="TRANSLATION" outputText={translation} num="2" />
+                        <LinguaSingleOutput logo={logo3} title="ASSOCIATION" outputText={association} isEven={true} num="3" />
+                        <LinguaSingleOutput logo={logo4} title="MENTAL IMAGE" outputText={mentalImage} num="4" />
+                        <LinguaSingleOutput logo={logo5} title="EXPLANATION" outputText={explanation} isEven={true} num="5" />
+                    </div>
+                </div>
+
+                <div className="ll-right-column">
+                    <h4 className="project-mini-title img-gen-title ll-title">IMAGE GENERATION</h4>
+                    <div className="ll-img-container">
+                        {url === "" && <p className='ll-img-msg'>Generate an image to help remember the association!</p>}
+                        {url !== "" && <img src={url}></img>}
+                    </div>
+                    <button 
+                        className="submit-btn get-img-btn ll-btn"
+                        onClick={() => generateImage(mentalImage, setUrl)}>
+                            Get Image
+                    </button>
+                </div>
             </div>
-
-            <button 
-                    className="submit-btn get-img-btn"
-                    onClick={() => generateImage(mentalImage, setUrl)}>
-                        Get Image
-                </button>
-            
-            {url !== "" && <img src={url}></img>}
-
         </div>
     </div>
     <Footer />
---
+++ src/pages/Projects/LinguaLink/SideBar.jsx
@@ -0,0 +1,65 @@
+import './SideBar.css'
+
+import React from 'react';
+import {
+  CDBSidebar,
+  CDBSidebarContent,
+  CDBSidebarFooter,
+  CDBSidebarHeader,
+} from 'cdbreact';
+
+function LLHistoryItem (item, i, updateOutput) {
+    return (
+        <div className="ll-history-item" key={i}>
+            <div className="square-container">
+                <div className={`square square-${i % 5}`}></div>
+            </div>
+            <div className="ll-history-item-container">
+                <span onClick={() => updateOutput(i)}><div className="ll-history-item-text">{item.word}</div></span>
+            </div>
+        </div>
+    )
+}
+
+const Sidebar = ( {history=[], updateOutput} ) => {
+  return (
+    <div className="ll-sidebar">
+      <CDBSidebar>
+        <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
+          <a href="/" className="text-decoration-none" style={{ color: 'inherit' }}>
+            History
+          </a>
+        </CDBSidebarHeader>
+
+        <CDBSidebarContent className="sidebar-content">
+            <div className="ll-history-sidebar">
+                {history.slice().reverse().map((item, i) => (
+                    LLHistoryItem(item, history.length - i - 1, updateOutput)
+                ))}
+
+                {/* <div className="ll-history-item">
+                    <div className="square-container">
+                        <div className="square"></div>
+                    </div>
+                    <div className="ll-history-item-container">
+                        <div className="ll-history-item-text">One</div>
+                    </div>
+                </div> */}
+            </div>
+        </CDBSidebarContent>
+
+        <CDBSidebarFooter style={{ textAlign: 'center' }}>
+          <div
+            style={{
+              padding: '20px 5px',
+            }}
+          >
+            {/* Sidebar Footer */}
+          </div>
+        </CDBSidebarFooter>
+      </CDBSidebar>
+    </div>
+  );
+};
+
+export default Sidebar;
